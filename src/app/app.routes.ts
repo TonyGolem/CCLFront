a83ac5e3b41108928245f5c10c8e9554853af3f1
David Alvarado <davidalvarado0814@hotmail.com>
2025-04-02 12:19:42 -0500

    Creacion de proyecto, Agregar componentes de autenticación e inventario con sus respectivas plantillas y estilos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './core/components/auth/auth/auth.component';
+import { InventarioComponent } from './core/components/content/inventario/inventario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'auth', pathMatch: 'full' },
+  { path: 'auth', component: AuthComponent },
+  {
+    path: 'inventario', component: InventarioComponent,
+    // canActivate: [AuthGuard]
+  },
+  { path: '**', redirectTo: 'login' }];
